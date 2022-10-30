dc2d5c6f89e79883c6675a246bc0699813753920
martinkkyalo10 <martinkkyalo10@gmail.com>
2022-10-30 03:40:36 +0300

    single product Api

---
+++ pages/api/products/[id].js
@@ -0,0 +1,39 @@
+import dbConnect from "../../../lib/DbConnect";
+import Product from "../../../models/Product";
+
+export default async function handler(req, res) {
+  const {
+    method,
+    query: { id },
+  } = req;
+
+  dbConnect();
+
+  if (method === "GET") {
+    try {
+      const product = await Product.findById(id);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  if (method === "PUT") {
+    try {
+      const product = await Product.create(req.body);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  if (method === "DELETE") {
+    try {
+      const product = await Product.create(req.body);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+  // res.status(200).json({ name: "John Doe" });
+}
